1829f2cb0c4f55b2afd3e647f7b1892744ae9f4c
João Roberto <jbetol@gmail.com>
2020-05-12 11:50:21 -0300

    Preparando missão 2

--- Cap05/Lab03/forca_v1.py
+++ Cap05/Lab03/forca_v1.py
@@ -71,32 +71,57 @@ class Hangman:
 
 	# Método Construtor
 	def __init__(self, word):
-		
-		
+		self.word = word
+		self.escondida = []
+		#inicializando escondida para mascarar word
+		for i in range(len(word)):
+			self.escondida.append('_')
+		self.erradas = " "
+		self.corretas = " "
+		self.game_status = 0 
+		self.print_game_status()
+
 	# Método para adivinhar a letra
 	def guess(self, letter):
-		
+		if letter in self.word:
+			self.corretas = self.corretas + " " + letter
+		else:
+			self.erradas = self.erradas + " "+ letter
+			self.game_status += 1
+
+		self.hide_word(letter)
+
 		
 	# Método para verificar se o jogo terminou
 	def hangman_over(self):
-		
+		return (self.game_status >= 6)
 		
 	# Método para verificar se o jogador venceu
 	def hangman_won(self):
+		return ("".join(self.escondida) == self.word) 		
+		
+	# Método para não mostrar a letra no board
+	def hide_word(self, letter):
+		for ind, char in enumerate(self.word):
+			if char == letter:
+				self.escondida[ind] = letter
 		
 
-	# Método para não mostrar a letra no board
-	def hide_word(self):
-		
-		
 	# Método para checar o status do game e imprimir o board na tela
 	def print_game_status(self):
+		print(board[self.game_status])
+		print("\n\nPalavra: " + "".join(self.escondida)) 
+		print("\n\nLetras erradas: "+ self.erradas)
+		print("\n\nLetras corretas: "+ self.corretas)
+
+		return (self.hangman_won() or self.hangman_over())
 		
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
         with open("palavras.txt", "rt") as f:
-                bank = f.readlines()
+            bank = f.readlines()
+
         return bank[random.randint(0,len(bank))].strip()
 
 
@@ -107,10 +132,12 @@ def main():
 	game = Hangman(rand_word())
 
 	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	
-
+	fim = False
+	while not fim:
+		letra = input("\n\nIndique uma letra -> ")
+		game.guess(letra)
 	# Verifica o status do jogo
-	game.print_game_status()	
+		fim = game.print_game_status()	
 
 	# De acordo com o status, imprime mensagem na tela para o usuário
 	if game.hangman_won():
